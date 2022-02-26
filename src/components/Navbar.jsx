b959b9059d6476d6a1fea24d3e4e9ba0205b3602
tGodson <tendongze95@gmail.com>
2022-02-26 01:10:23 +0100

    initialise app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BuldOutlined, FundOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            
+        </div>
+    )
+}
+
+export default Navbar
